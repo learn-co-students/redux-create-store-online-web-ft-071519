3da26c239011410d6cb086d104f9a3b4b77dc501
github username <github email address>
2019-12-02 11:37:26 +0000

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,44 @@
-let state;
 
-function reducer(state = { count: 0 }, action) {
+
+let button = document.getElementById('button');
+
+const createStore = (reducer) =>{
+  
+    let state;
+
+    const dispatch = action => {
+      state = reducer(state, action);
+      render();
+    };
+
+    const getState = () => state;
+
+    return {
+      dispatch,
+      getState
+    }; //to store outside the function. sort od similar way to export
+
+}
+
+const reducer = (state = {count: 0}, action) => {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
-
     default:
-      return state;
+      return state; 
   }
-};
+} 
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
 
-function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+const render = () => {
+  let container = document.getElementById("container");
+  container.textContent = store.getState().count;
+}
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+let store = createStore(reducer);
+store.dispatch({type: "@@INIT"});
+console.log(store.getState().count);
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+button.addEventListener("click", () => {store.dispatch({ type: 'INCREASE_COUNT' })});
+
+
